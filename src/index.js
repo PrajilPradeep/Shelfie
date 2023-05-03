b7ed90173e2903b9ceaa2218fdeb0558f63655c4
Prajil <Prajil7425@gmail.com>
2023-05-03 13:30:02 +0530

    Add Logo and Buttons

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Logo } from "./components/logo.js";
 
-function App(){
-  return <div>Hello</div>
+function App() {
+  return (
+    <div>
+      <Logo width="80" height="80" />
+      <h1>Shelfie</h1>
+      <div>
+        <button onClick={() => alert("Login clicked")}>Login</button>
+      </div>
+      <div>
+        <button onClick={() => alert("Register clicked")}>Register</button>
+      </div>
+    </div>
+  );
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-
